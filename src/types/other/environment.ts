ec5630c7d7b2a29e3a517922bc8bde3bbbfec81e
Katarzyna <otiofthebag@protonmail.com>
2023-01-16 15:27:36 +0100

    added documentation

--- src/types/other/environment.ts
+++ src/types/other/environment.ts
@@ -6,6 +6,7 @@ declare global {
          * Interface for .env file to be recognizable by TypeScript
          */
         interface ProcessEnv {
+            [key: string]: any;
             JWT_KEY: string;
             PORT: number;
             DB_HOST: string;
